2484f0e6186ec7e9b56ae7c7e6d2970324f52169
Sathish Kumar <crazysathish02@gmail.com>
2022-05-27 07:20:50 +0530

    Udbav Lights Website

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+echo nl2br("Thank You!\r\n\n We Are Happy To Serve You!");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body><br><br>
+    <a href="index.html">Home</a>
+</body>
+</html>
